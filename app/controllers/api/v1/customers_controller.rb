7d78c8fb15324eadc7dbb5bac1e5492c2e7e8cd2
benoit <torrentdu64@gmail.com>
2025-01-28 14:09:34 +1300

    BUGFIX total_count return correct amount

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -14,7 +14,7 @@ class Api::V1::CustomersController < ApplicationController
         next_page: customers.next_page,
         prev_page: customers.prev_page,
         total_pages: customers.total_pages,
-        total_count: customers.total_count
+        total_count: Customer.count
       }
     }
   end
